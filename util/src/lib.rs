a8f94a62318fd7c3d82a3b51f3ace22c2e77f9d8
Gabriel Vogel <gabriel.vogel@online.de>
2025-11-05 21:09:28 +0100

    util: Remove input search from source file

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,12 +1,10 @@
 use std::path::PathBuf;
 use std::{env, fs, io};
 
-fn find_input_dir(this_file: &str) -> io::Result<PathBuf> {
-    let srcpath = ::std::path::Path::new(this_file).parent().unwrap();
+fn find_input_dir() -> io::Result<PathBuf> {
     let cwd = env::current_dir()?;
     cwd.ancestors()
-        .chain(srcpath.ancestors())
-        .map(|p| dbg!(p).join("input"))
+        .map(|p| p.join("input"))
         .find(|p| p.is_dir())
         .ok_or(io::Error::new(
             io::ErrorKind::NotFound,
@@ -14,8 +12,8 @@ fn find_input_dir(this_file: &str) -> io::Result<PathBuf> {
         ))
 }
 
-fn input_file(fname: &str, part: u32, file: &str) -> io::Result<PathBuf> {
-    let dir = find_input_dir(file)?;
+fn input_file(fname: &str, part: u32) -> io::Result<PathBuf> {
+    let dir = find_input_dir()?;
     let direct = dir.join(fname);
     if direct.is_file() {
         Ok(direct)
@@ -25,20 +23,19 @@ fn input_file(fname: &str, part: u32, file: &str) -> io::Result<PathBuf> {
         let suffix = fname.to_string() + &part_s + ".txt";
         let full_fname = dir
             .read_dir()?
-            .filter(|der| {
+            .find(|der| {
                 der.as_ref().is_ok_and(|de| {
                     de.file_type().is_ok_and(|ft| ft.is_file())
                         && de.file_name().to_string_lossy().ends_with(&suffix)
                 })
             })
-            .next()
             .ok_or(io::Error::from(io::ErrorKind::NotFound))??
             .file_name();
         Ok(dir.join(full_fname))
     }
 }
 
-pub fn get_input(fname: &str, file: &str) -> io::Result<(String, u32)> {
+pub fn get_input(fname: &str) -> io::Result<(String, u32)> {
     let mut part = 1;
     let mut input_path = None;
     for arg in env::args_os().skip(1) {
@@ -52,7 +49,7 @@ pub fn get_input(fname: &str, file: &str) -> io::Result<(String, u32)> {
     }
     let path = match input_path {
         Some(p) => p.into(),
-        None => input_file(fname, part, file)?,
+        None => input_file(fname, part)?,
     };
     let input = fs::read_to_string(path)?;
     Ok((input, part))
@@ -71,8 +68,7 @@ macro_rules! evc_main {
     };
     ( $x:expr ) => {
         fn main() -> std::io::Result<()> {
-            let this_file = ::std::file!();
-            let (input, part) = util::get_input($x, this_file)?;
+            let (input, part) = util::get_input($x)?;
             match part {
                 1 => part1(input),
                 2 => part2(input),
